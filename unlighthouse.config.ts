9ebcaa77cee42f9b7c8bb5249a8ffe827e9be825
Adam Lipert <a.lipert@protonmail.com>
2023-05-09 08:35:38 +0200

    Try custom publish

---
+++ unlighthouse.config.ts
@@ -0,0 +1,11 @@
+export default {
+    site: 'https://ortieez.github.io/Vodafone-microsite/',
+    ci: {
+      budget: {
+        performance: 50,
+        accessibility: 100,
+        'best-practices': 90,
+        seo: 90,
+      }
+    }
+  }
